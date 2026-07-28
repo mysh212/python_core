f9b0a2b2bb41e0a352a998b6f8135c69870acd12
ysh <mysh212@gmail.com>
2026-07-28 10:13:20 +0800

    Add IP damper
     - [ADD] IP damper into `web/request` function

--- web.py
+++ web.py
@@ -3,6 +3,9 @@
 from core.general import *
 import core.log as log
 
+from datetime import datetime
+from collections import defaultdict
+
 info = warning = lambda *x, **y: None
 
 def ok(data = None, custom: bool = False, **ot) -> dict:
@@ -44,8 +47,29 @@ def fail(error = None) -> dict:
         'error': error
     }
 
-def require(request, f: list) -> bool:
+class damper:
+    def __init__(self):
+        self.f = defaultdict(lambda: defaultdict(float))
+        pass
+
+    def access(self, IP: str, name: str = '', delay: int = 0):
+        now = datetime.now().timestamp()
+        pre = self.f[name][IP]
+        if abs(now - pre) < delay:
+            return False
+        self.f[name][IP] = now
+        return True
+
+    def clear(self):
+        self.f.clear()
+        return
+
+damp = damper()
+
+def require(request, f: list, delay: int = 0, name: str = '') -> bool:
+    if not damp.access(request.remote_addr, name, delay):
+        return False
     for i in f:
         if i not in request.values:
             return False
-    return True
+    return [request.values[i] for i in f]
